45efe04369b5f5f2a2898da41e1ce37e94bd68a7
Jawad Rashid <67164279+Jawadxoxo38@users.noreply.github.com>
2023-11-26 22:56:47 -0500

    Update dataBase.cpp

--- dataBase.cpp
+++ dataBase.cpp
@@ -1,1 +1,31 @@
-#include "dataBase.h"
+#include "Database.h"
+
+Database::Database() {
+    // Constructor implementation 
+}
+
+void Database::makeFile(const Inventory& inventory) {
+    std::ofstream outputFile("dbfile"); // Will have to add a file 
+
+    if (outputFile.is_open()) {
+        for (int i = 0; i < inventory.GetSize(); ++i) {
+            outputFile << inventory.GetItem(i).GetName() << ","
+                       << inventory.GetItem(i).GetPrice() << ","
+                       << inventory.GetItem(i).GetType() << "\n";
+            // output 
+        }
+        outputFile.close();
+    } else {
+        std::cout << "Unable to create database file!" << std::endl;
+        // Implement appropriate error handling here
+    }
+}
+
+void Database::updateFile(const Inventory& inventory) {
+    // Implement updating the existing database with Inventory changes
+    // Similar to makeFile but for updating changes
+}
+
+void Database::printFile() const {
+    // Printing details 
+}
